85557330ca1a2184cd179fdc4d75bd21e45bb3b7
rafael.hirooka <rafaelhirooka@vegas.com.br>
2018-03-13 18:54:14 -0300

    Atualização antes de ir embora para continuar em casa

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+ * Nesse caso nessa rota o id é obrigatório ser passado na url
+ */
+
+/*
+Route::get('/contato/{id}', function ($id) {
+    return "Contato " . $id;
+});
+*/
+
+
+/*
+ * Nesse caso nessa rota o id não é obrigatório
+ */
+Route::get('/contato/{id?}', function ($id = NULL) {
+    if ($id != NULL) {
+        return "Tem o id e ele vale " . $id;
+    } else {
+        return "O id não foi passado";
+    }
+});
+
+Route::post('/contato', function () {
+
+});
